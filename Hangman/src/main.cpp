eb438796f87c1b3c5212972c16b95b908d5ab22d
shawn220a <shawn.42496@gmail.com>
2020-11-06 16:24:52 -0600

    Finish Game Play

--- Hangman/src/main.cpp
+++ Hangman/src/main.cpp
@@ -8,8 +8,12 @@ void PlayGame();
 bool WantToPlayAgain();
 int GetSecretPhrase(char secretPhrase[], int maxLength);
 char *MakeHiddenPhrase(const char *secretPhrase, int secretPhraseLength);
-void DrawBoard(int numberOfGuessesLeft, const char *optrHiddenPhrase);
-
+void DrawBoard(int numberOfGuessesLeft, const char *noptrHiddenPhrase);
+char GetGuess();
+void UpdateBoard(char guess, char *noptrHiddenPhrase, const char secretPhrase[], int secretPhraseLength,
+                 int &numberOfGuessesLeft);
+bool IsGameOver(int numberOfGuessesLeft, const char *noptrHiddenPhrase, int secretPhraseLength);
+void DisplayResult(const char *secretPhrase, int numberOfGuessesLeft);
 char GetCharacter(const char *prompt, const char *error);
 char GetCharacter(const char *prompt, const char *error, const char validInput[], int validInputLength);
 
@@ -40,6 +44,21 @@ void PlayGame()
     optrHiddenPhrase = MakeHiddenPhrase(secretPhrase, secretPhraseLength);
 
     DrawBoard(numberOfGuessesLeft, optrHiddenPhrase);
+
+    char guess;
+
+    do
+    {
+        guess = GetGuess();
+
+        UpdateBoard(guess, optrHiddenPhrase, secretPhrase, secretPhraseLength, numberOfGuessesLeft);
+
+        DrawBoard(numberOfGuessesLeft, optrHiddenPhrase);
+    } while (!IsGameOver(numberOfGuessesLeft, optrHiddenPhrase, secretPhraseLength));
+
+    DisplayResult(secretPhrase, numberOfGuessesLeft);
+
+    delete[] optrHiddenPhrase;
 }
 
 bool WantToPlayAgain()
@@ -47,7 +66,7 @@ bool WantToPlayAgain()
     const char validInputs[] = {'y', 'n'};
 
     char response = GetCharacter("Would you like to play again? ", INPUT_ERROR_STRING, validInputs, 2);
-    
+
     return response == 'y';
 }
 
@@ -104,7 +123,7 @@ char *MakeHiddenPhrase(const char *secretPhrase, int secretPhraseLength)
     return hiddenPhrase;
 }
 
-void DrawBoard(int numberOfGuessesLeft, const char *optrHiddenPhrase)
+void DrawBoard(int numberOfGuessesLeft, const char *noptrHiddenPhrase)
 {
     switch (numberOfGuessesLeft)
     {
@@ -175,8 +194,56 @@ void DrawBoard(int numberOfGuessesLeft, const char *optrHiddenPhrase)
         break;
     }
 
-    cout << "Secret Phrase: " << optrHiddenPhrase << endl
+    cout << "Secret Phrase: " << noptrHiddenPhrase << endl
          << endl;
+}
+
+char GetGuess()
+{
+    return GetCharacter("Please input your guess: ", INPUT_ERROR_STRING);
+}
+
+void UpdateBoard(char guess, char *noptrHiddenPhrase, const char secretPhrase[], int secretPhraseLength,
+                 int &numberOfGuessesLeft)
+{
+    bool found = false;
+
+    for (int i = 0; i < secretPhraseLength; i++)
+    {
+        if (tolower(secretPhrase[i]) == guess)
+        {
+            noptrHiddenPhrase[i] = secretPhrase[i];
+
+            found = true;
+        }
+    }
+
+    if (!found)
+        numberOfGuessesLeft--;
+}
+
+bool IsGameOver(int numberOfGuessesLeft, const char *noptrHiddenPhrase, int secretPhraseLength)
+{
+    bool hasDash = false;
+
+    for (int i = 0; i < secretPhraseLength; i++)
+    {
+        if (noptrHiddenPhrase[i] == '-')
+        {
+            hasDash = true;
+            break;
+        }
+    }
+
+    return numberOfGuessesLeft == 0 || !hasDash;
+}
+
+void DisplayResult(const char *secretPhrase, int numberOfGuessesLeft)
+{
+    if (numberOfGuessesLeft > 0)
+        cout << "You got it! The phrase was: " << secretPhrase << endl;
+    else
+        cout << "You didn't get it.... The phrase was: " << secretPhrase << endl;
 }
 
 char GetCharacter(const char *prompt, const char *error)
