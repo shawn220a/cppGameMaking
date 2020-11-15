596717ad0c0d01ba4d4fddb8e9b879b58e432ecd
shawn220a <shawn.42496@gmail.com>
2020-11-15 16:55:26 -0600

    Format code in main.cpp

--- Battleship/src/main.cpp
+++ Battleship/src/main.cpp
@@ -26,23 +26,19 @@ int main()
     PlayGame(player1, player2);
   } while (WantToPlayAgain());
 
-
   return 0;
 }
 
 void InitializePlayer(Player& player, const char* playerName)
 {
   if (playerName != nullptr && strlen(playerName) > 0)
-  {
     strcpy(player.playerName, playerName);
-  }
 
   InitializeShip(player.ships[0], AIRCRAFT_CARRIER_SIZE, ST_AIRCRAFT_CARRIER);
   InitializeShip(player.ships[1], BATTLESHIP_SIZE, ST_BATTLESHIP);
   InitializeShip(player.ships[2], CRUISER_SIZE, ST_CRUISER);
   InitializeShip(player.ships[3], DESTROYER_SIZE, ST_DESTROYER);
   InitializeShip(player.ships[4], SUBMARINE_SIZE, ST_SUBMARINE);
-
 }
 
 void InitializeShip(Ship& ship, int shipSize, ShipType shipType)
